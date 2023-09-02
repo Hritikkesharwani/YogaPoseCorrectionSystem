ad1ceac368015ce4f74adcae36d9e34624b30c52
HritikKesharwani <sanjukesharwani2000@gmail.com>
2023-09-02 19:52:56 +0530

    added yoga component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />}/>
-        {/*<Route path='/start' element={<Yoga />} />*/}
+        <Route path='/start' element={<Yoga />} />
         <Route path='/about' element={<About />} />
         <Route path='/tutorials' element={<Tutorials />} />
       </Routes>
